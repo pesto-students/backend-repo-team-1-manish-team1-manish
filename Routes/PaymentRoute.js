ac28d4193f4ba9c034aefe7600cd7bbbfc70e4ad
Pranav Srisrimal <pranavsrisrimal@gmail.com>
2023-07-08 00:38:36 +0530

    feat : checkout, validate routes and packages

--- Routes/PaymentRoute.js
+++ Routes/PaymentRoute.js
@@ -1,7 +1,14 @@
-const express = require('express');
-const Razorpay = require('razorpay');
+const express = require("express");
+const Razorpay = require("razorpay");
 const router = express.Router();
-require('dotenv').config();
+const cors = require("cors");
+const bodyParser = require("body-parser");
+const shortid = require("shortid");
+require("dotenv").config();
+
+router.use(bodyParser.json());
+router.use(bodyParser.urlencoded({ extended: true }));
+router.use(cors());
 
 const { KEY_ID, KEY_SECRET } = process.env;
 
@@ -10,20 +17,53 @@ let rzp = new Razorpay({
   key_secret: KEY_SECRET,
 });
 
-//Sample items
-const storeItems = new Map([
-  [1, { price: 10000, name: "Item 1" }],
-  [2, { price: 20000, name: "Item 2" }],
-])
+// Verification
+router.post("/verification", (req, res) => {
+  // do a validation
+  const secret = "WEBHOOK_SECRET";
+  console.log(req.body);
 
-// API signature
-// {razorpayInstance}.{resourceName}.{methodName}(resourceId [, params])
+  const crypto = require("crypto");
 
-//Route to direct customer to checkout page - Razorpay
+  const shasum = crypto.createHmac("sha256", secret);
+  shasum.update(JSON.stringify(req.body));
+  const digest = shasum.digest("hex");
+
+  console.log(digest, req.headers["x-razorpay-signature"]);
+
+  if (digest === req.headers["x-razorpay-signature"]) {
+    console.log("Request verified");
+    console.log(JSON.stringify(req.body, null, 4));
+    // Write to db (optional)
+  } else {
+    // ignore or pass it
+  }
+  res.json({ status: "ok" });
+});
+
+// Checkout
 router.post("/checkout", async (req, res) => {
-    const session = await rzp.payments.capture(order_id, 1000).then((data) => {
-        // console.log(data); SUCCESS
-      }).catch((error) => {
-          res.status(500).json({ error : error.message })
-        })
-    })
+  const payment_capture = 1;
+  const amount = 100;
+  const currency = "INR";
+
+  const options = {
+    amount: amount * 100,
+    currency,
+    receipt: shortid.generate(),
+    payment_capture,
+  };
+
+  try {
+    const response = await rzp.orders.create(options);
+    console.log(response);
+    res.json({
+      id: response.id,
+      currency: response.currency,
+      amount: response.amount,
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).send({ message: "Failed to create payment" });
+  }
+});
