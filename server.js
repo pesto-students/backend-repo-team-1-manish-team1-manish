555a53953a39aa3f95e30b6f371ca7e90f991e6c
SaurabhK05 <saurabh.k150600@gmail.com>
2023-07-09 13:30:35 +0530

    add CORS header

--- server.js
+++ server.js
@@ -56,6 +56,12 @@ app.use(
     pathRewrite: {
       "^/api": "",
     },
+    headers: {
+      "Access-Control-Allow-Origin": "*",
+      "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, PATCH, OPTIONS",
+      "Access-Control-Allow-Headers":
+        "X-Requested-With, content-type, Authorization",
+    },
   })
 );
 
