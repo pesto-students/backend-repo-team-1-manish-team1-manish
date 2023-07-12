ddc9954b0ffa3a33a282c3017b91d0da75765d91
SaurabhK05 <saurabh.k150600@gmail.com>
2023-07-13 02:02:56 +0530

    add car-deatil route

--- Models/CarDetailsModel.js
+++ Models/CarDetailsModel.js
@@ -3,276 +3,353 @@ const sql = require("../DB/PostgresSql");
 const CarDetails = {};
 
 // CREATE CAR DETAILS
-CarDetails.create = async (brand, year, model, fuelType, fuelCapacity, registrationYear, engine, variant, ownership, kmDriven, transmission, transmissionShort, insurance, pinCode, registrationState, city, registrationNumber, sellerId, buyerId, nearestRtoOffice, price, type, tags, images, carApiId) => {
-    try {
-        await sql`
+CarDetails.create = async (
+  brand,
+  year,
+  model,
+  fuelType,
+  fuelCapacity,
+  registrationYear,
+  engine,
+  variant,
+  ownership,
+  kmDriven,
+  transmission,
+  transmissionShort,
+  insurance,
+  pinCode,
+  registrationState,
+  city,
+  registrationNumber,
+  sellerId,
+  buyerId,
+  nearestRtoOffice,
+  price,
+  type,
+  tags,
+  images,
+  carApiId
+) => {
+  try {
+    await sql`
       INSERT INTO car_details (Id, Brand, Year, Model, FualType, FualCapacity, RegistrationYear, Engine, Variant, Ownership, KmDriven, Transmission, TransmissionShort, Insaurance, PinCode, RegistrationState, City, RegistrationNumber, SellerId, BuyerId, NearestRtoOffice, Price, Type, Tags, Images, carApiId)
       VALUES (gen_random_uuid(), ${brand}, ${year}, ${model}, ${fuelType}, ${fuelCapacity}, ${registrationYear}, ${engine}, ${variant}, ${ownership}, ${kmDriven}, ${transmission}, ${transmissionShort}, ${insurance}, ${pinCode}, ${registrationState}, ${city}, ${registrationNumber}, ${sellerId}, ${buyerId}, ${nearestRtoOffice}, ${price}, ${type}, ${tags}, ${images}, ${carApiId})
     `;
 
-        const newCarDetails = await CarDetails.getByRegistrationNumber(registrationNumber);
-        return newCarDetails;
-    } catch (error) {
-        console.error('Error creating car details:', error);
-        throw error;
-    }
+    const newCarDetails = await CarDetails.getByRegistrationNumber(
+      registrationNumber
+    );
+    return newCarDetails;
+  } catch (error) {
+    console.error("Error creating car details:", error);
+    throw error;
+  }
 };
 
 // GET ALL CAR DETAILS
 CarDetails.get = async () => {
-    try {
-        return await sql`
+  try {
+    return await sql`
       SELECT * FROM car_details
     `;
-    } catch (error) {
-        console.error('Error getting car details:', error);
-        throw error;
-    }
+  } catch (error) {
+    console.error("Error getting car details:", error);
+    throw error;
+  }
 };
 
 // GET CAR DETAILS BY ID
 CarDetails.getById = async (id) => {
-    try {
-        return (await sql`
+  try {
+    return (
+      await sql`
       SELECT * FROM car_details WHERE Id = ${id}
-    `)[0];
-    } catch (error) {
-        console.error('Error getting car details by ID:', error);
-        throw error;
-    }
+    `
+    )[0];
+  } catch (error) {
+    console.error("Error getting car details by ID:", error);
+    throw error;
+  }
 };
 
 // GET CAR DETAILS BY REGISTRATION NUMBER
 CarDetails.getByRegistrationNumber = async (registrationNumber) => {
-    try {
-        return (await sql`
+  try {
+    return (
+      await sql`
       SELECT * FROM car_details WHERE RegistrationNumber = ${registrationNumber}
-    `)[0];
-    } catch (error) {
-        console.error('Error getting car details by registration number:', error);
-        throw error;
-    }
+    `
+    )[0];
+  } catch (error) {
+    console.error("Error getting car details by registration number:", error);
+    throw error;
+  }
 };
 
 // UPDATE CAR DETAILS
 CarDetails.update = async (
-    id,
-    {
-        fuelType,
-        fuelCapacity,
-        registrationYear,
-        ownership,
-        kmDriven,
-        transmission,
-        transmissionShort,
-        insurance,
-        registrationState,
-        registrationNumber,
-        nearestRtoOffice,
-        images
-    } = {}
+  id,
+  {
+    fuelType,
+    fuelCapacity,
+    registrationYear,
+    ownership,
+    kmDriven,
+    transmission,
+    transmissionShort,
+    insurance,
+    registrationState,
+    registrationNumber,
+    nearestRtoOffice,
+    images,
+  } = {}
 ) => {
-    try {
-        let query = sql`UPDATE car_details SET Id = ${id}
+  try {
+    let query = sql`UPDATE car_details SET Id = ${id}
         ${fuelType ? sql`, FualType = ${fuelType}` : sql``}
         ${fuelCapacity ? sql`, FualCapacity = ${fuelCapacity}` : sql``}
-        ${registrationYear ? sql`, RegistrationYear = ${registrationYear}` : sql``}
+        ${
+          registrationYear
+            ? sql`, RegistrationYear = ${registrationYear}`
+            : sql``
+        }
         ${ownership ? sql`, Ownership = ${ownership}` : sql``}
         ${kmDriven ? sql`, KmDriven = ${kmDriven}` : sql``}
         ${transmission ? sql`, Transmission = ${transmission}` : sql``}
-        ${transmissionShort ? sql`, TransmissionShort = ${transmissionShort}` : sql``}
+        ${
+          transmissionShort
+            ? sql`, TransmissionShort = ${transmissionShort}`
+            : sql``
+        }
         ${insurance ? sql`, Insaurance = ${insurance}` : sql``}
-        ${registrationState ? sql`, RegistrationState = ${registrationState}` : sql``}
-        ${registrationNumber ? sql`, RegistrationNumber = ${registrationNumber}` : sql``}
-        ${nearestRtoOffice ? sql`, NearestRtoOffice = ${nearestRtoOffice}` : sql``}
+        ${
+          registrationState
+            ? sql`, RegistrationState = ${registrationState}`
+            : sql``
+        }
+        ${
+          registrationNumber
+            ? sql`, RegistrationNumber = ${registrationNumber}`
+            : sql``
+        }
+        ${
+          nearestRtoOffice
+            ? sql`, NearestRtoOffice = ${nearestRtoOffice}`
+            : sql``
+        }
         ${images ? sql`, Images = ${images}` : sql``}
         WHERE Id = ${id}
         `;
-        return await query;
-    } catch (error) {
-        console.error('Error updating car details:', error);
-        throw error;
-    }
+    return await query;
+  } catch (error) {
+    console.error("Error updating car details:", error);
+    throw error;
+  }
 };
 
 // DELETE CAR DETAILS
 CarDetails.delete = async (id) => {
-    try {
-        return await sql`
+  try {
+    return await sql`
       DELETE FROM car_details WHERE Id = ${id}
     `;
-    } catch (error) {
-        console.error('Error deleting car details:', error);
-        throw error;
-    }
+  } catch (error) {
+    console.error("Error deleting car details:", error);
+    throw error;
+  }
 };
-
 
 // BUY CAR
 CarDetails.buy = async (id, buyerId) => {
-    try {
-        let query = sql`
+  try {
+    let query = sql`
       UPDATE car_details SET BuyerId = ${buyerId} WHERE Id = ${id}
     `;
 
-        return await query;
-    } catch (error) {
-        console.error('Error buying car:', error);
-        throw error;
-    }
+    return await query;
+  } catch (error) {
+    console.error("Error buying car:", error);
+    throw error;
+  }
 };
 
 // Fetch car brands for a range of prices
 CarDetails.getBrandsByPriceRange = async (minPrice, maxPrice) => {
-    try {
-        return await sql`
+  try {
+    return await sql`
       SELECT DISTINCT Brand FROM car_details WHERE Price >= ${minPrice} AND Price <= ${maxPrice}
     `;
-    } catch (error) {
-        console.error('Error getting car brands by price range:', error);
-        throw error;
-    }
+  } catch (error) {
+    console.error("Error getting car brands by price range:", error);
+    throw error;
+  }
 };
 
 // Fetch car types by price range and brand
-CarDetails.getCarTypesByPriceRangeAndBrand = async (minPrice, maxPrice, brand) => {
-    try {
-        return await sql`
+CarDetails.getCarTypesByPriceRangeAndBrand = async (
+  minPrice,
+  maxPrice,
+  brand
+) => {
+  try {
+    return await sql`
       SELECT DISTINCT Type FROM car_details WHERE Price >= ${minPrice} AND Price <= ${maxPrice} AND Brand = ${brand}
     `;
-    } catch (error) {
-        console.error('Error getting car types by price range and brand:', error);
-        throw error;
-    }
+  } catch (error) {
+    console.error("Error getting car types by price range and brand:", error);
+    throw error;
+  }
 };
 
 // Fetch car details by price range, brand, and car type
-CarDetails.getCarDetailsByPriceRangeBrandAndType = async (minPrice, maxPrice, brand, type) => {
-    try {
-        return await sql`
+CarDetails.getCarDetailsByPriceRangeBrandAndType = async (
+  minPrice,
+  maxPrice,
+  brand,
+  type
+) => {
+  try {
+    return await sql`
       SELECT * FROM car_details WHERE Price >= ${minPrice} AND Price <= ${maxPrice} AND Brand = ${brand} AND Type = ${type}
     `;
-    } catch (error) {
-        console.error('Error getting car details by price range, brand, and type:', error);
-        throw error;
-    }
+  } catch (error) {
+    console.error(
+      "Error getting car details by price range, brand, and type:",
+      error
+    );
+    throw error;
+  }
 };
 
 // Fetch all car brands
 CarDetails.getAllCarBrands = async () => {
-    try {
-        let query = sql`SELECT DISTINCT Brand FROM car_details`;
-        return await query;
-    } catch (error) {
-        console.error('Error getting all car brands:', error);
-        throw error;
-    }
+  try {
+    let query = sql`SELECT DISTINCT Brand FROM car_details`;
+    return await query;
+  } catch (error) {
+    console.error("Error getting all car brands:", error);
+    throw error;
+  }
 };
 
 // Fetch car models with their brand
 CarDetails.getCarModelsWithBrand = async () => {
-    try {
-        return await sql`
+  try {
+    return await sql`
       SELECT Brand, Model FROM car_details
     `;
-    } catch (error) {
-        console.error('Error getting car models with brand:', error);
-        throw error;
-    }
+  } catch (error) {
+    console.error("Error getting car models with brand:", error);
+    throw error;
+  }
 };
 
 // Fetch car details based on optional query parameters
-CarDetails.getCarDetailsWithOptionalParameters = async (brands, minPrice, maxPrice, models, year, fuelType, kmDriven, transmission, nearestRtoOffice) => {
-    try {
-        let query = sql`SELECT * FROM car_details WHERE 1=1
-      ${brands && brands[0].length > 0 ? sql` AND Brand = ANY (${brands})` : sql``}
+CarDetails.getCarDetailsWithOptionalParameters = async (
+  brands,
+  minPrice,
+  maxPrice,
+  models,
+  year,
+  fuelType,
+  kmDriven,
+  transmission,
+  nearestRtoOffice
+) => {
+  //   console.log(brands, minPrice, maxPrice, models);
+  try {
+    let query = sql`SELECT * FROM car_details WHERE 1=1
+      ${brands && brands[0].length > 0 ? sql` AND Brand =  (${brands})` : sql``}
       ${minPrice ? sql` AND Price >= ${minPrice}` : sql``}
       ${maxPrice ? sql` AND Price <= ${maxPrice}` : sql``}
-      ${models && models[0].length > 0 ? sql` AND Model = ANY (${models})` : sql``}
+      ${models && models[0].length > 0 ? sql` AND Model =  (${models})` : sql``}
       ${year ? sql` AND Year = ${year}` : sql``}
       ${fuelType ? sql` AND FualType = ${fuelType}` : sql``}
       ${kmDriven ? sql` AND KmDriven = ${kmDriven}` : sql``}
       ${transmission ? sql` AND Transmission = ${transmission}` : sql``}
-      ${nearestRtoOffice ? sql` AND NearestRtoOffice = ${nearestRtoOffice}` : sql``}
+      ${
+        nearestRtoOffice
+          ? sql` AND NearestRtoOffice = ${nearestRtoOffice}`
+          : sql``
+      }
       `;
-
-        return await query;
-    } catch (error) {
-        console.error('Error getting car details with optional parameters:', error);
-        throw error;
-    }
+    // console.log(query);
+    return await query;
+  } catch (error) {
+    console.error("Error getting car details with optional parameters:", error);
+    throw error;
+  }
 };
 
 // GET AVAILABLE CAR FUEL TYPES AND COUNT
 CarDetails.getAvailableFuelTypes = async () => {
-    try {
-        return await sql`
+  try {
+    return await sql`
         SELECT FuelType, COUNT(*) AS CarCount
         FROM car_details
         GROUP BY FuelType
       `;
-    } catch (error) {
-        console.error('Error fetching available car fuel types:', error);
-        throw error;
-    }
+  } catch (error) {
+    console.error("Error fetching available car fuel types:", error);
+    throw error;
+  }
 };
 
 // GET AVAILABLE CAR TYPES AND COUNT
 CarDetails.getAvailableCarTypes = async () => {
-    try {
-        return await sql`
+  try {
+    return await sql`
         SELECT Type, COUNT(*) AS CarCount
         FROM Car_Details
         GROUP BY Type
       `;
-    } catch (error) {
-        console.error('Error fetching available car types:', error);
-        throw error;
-    }
+  } catch (error) {
+    console.error("Error fetching available car types:", error);
+    throw error;
+  }
 };
 
 // GET TRANSMISSION COUNT
 CarDetails.getTransmissionCount = async () => {
-    try {
-        return await sql`
+  try {
+    return await sql`
         SELECT Transmission, COUNT(*) AS CarCount
         FROM Car_Details
         WHERE Transmission IN ('Automatic', 'Manual')
         GROUP BY Transmission
       `;
-    } catch (error) {
-        console.error('Error fetching car transmission count:', error);
-        throw error;
-    }
+  } catch (error) {
+    console.error("Error fetching car transmission count:", error);
+    throw error;
+  }
 };
 
 // GET AVAILABLE OWNERSHIPS AND COUNT
 CarDetails.getAvailableOwnerships = async () => {
-    try {
-        return await sql`
+  try {
+    return await sql`
         SELECT Ownership, COUNT(*) AS CarCount
         FROM Car_Details
         GROUP BY Ownership
       `;
-    } catch (error) {
-        console.error('Error fetching available ownerships:', error);
-        throw error;
-    }
+  } catch (error) {
+    console.error("Error fetching available ownerships:", error);
+    throw error;
+  }
 };
 
 // GET ALL UNIQUE AVAILABLE RTO OFFICES AND COUNT
 CarDetails.getAvailableRTOOffices = async () => {
-    try {
-        return await sql`
+  try {
+    return await sql`
         SELECT NearestRtoOffice, COUNT(*) AS CarCount
         FROM Car_Details
         GROUP BY NearestRtoOffice
       `;
-    } catch (error) {
-        console.error('Error fetching available RTO offices:', error);
-        throw error;
-    }
+  } catch (error) {
+    console.error("Error fetching available RTO offices:", error);
+    throw error;
+  }
 };
-
 
 module.exports = CarDetails;
--- server.js
+++ server.js
@@ -1,37 +1,40 @@
-const express = require('express');
-const cookieParser = require('cookie-parser');
-const bodyParser = require('body-parser');
-const authMiddleware = require('./Middleware/AuthService');
-const userOAuthRouter = require('./Routes/OAuthRoute');
-const carsApiRouter = require('./Routes/CarsApiRoute');
-const carsRouter = require('./Routes/CarsRoute');
-const userAuthRouter = require('./Routes/AuthRoute');
-const passport = require('passport');
-const cors = require('cors');
-const cookieSession = require('cookie-session');
+const express = require("express");
+const cookieParser = require("cookie-parser");
+const bodyParser = require("body-parser");
+const authMiddleware = require("./Middleware/AuthService");
+const userOAuthRouter = require("./Routes/OAuthRoute");
+const carsApiRouter = require("./Routes/CarsApiRoute");
+const carsRouter = require("./Routes/CarsRoute");
+const userAuthRouter = require("./Routes/AuthRoute");
+const passport = require("passport");
+const cors = require("cors");
+const cookieSession = require("cookie-session");
 
-require('dotenv').config();
-const port = process.env.PORT || 5000
+require("dotenv").config();
+const port = process.env.PORT || 5000;
 const app = express();
-app.use(cookieSession({
-    name: 'session',
-    keys: ['carbazaar'],
+app.use(
+  cookieSession({
+    name: "session",
+    keys: ["carbazaar"],
     maxAge: 24 * 60 * 60 * 100,
-}));
-app.use(cookieParser())
+  })
+);
+app.use(cookieParser());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(passport.initialize());
 app.use(passport.session());
-require('./DB/PostgresSql');
+require("./DB/PostgresSql");
 app.use(
-    cors({
-        origin: process.env.CORS_URL,
-        credentials: true,
-        optionSuccessStatus: 200,
-        methods: "GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS",
-        allowedHeaders: "Content-type,Accept,Access-Control-Allow-Credentials,Access-Control-Allow-Origin"
-    })
+  cors({
+    origin: process.env.CORS_URL,
+    credentials: true,
+    optionSuccessStatus: 200,
+    methods: "GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS",
+    allowedHeaders:
+      "Content-type,Accept,Access-Control-Allow-Credentials,Access-Control-Allow-Origin",
+  })
 );
 
 app.use("/auth", userOAuthRouter);
@@ -40,5 +43,4 @@ app.use("/cars-api", carsApiRouter);
 app.use(authMiddleware.verifyToken);
 app.use("/auth", userAuthRouter);
 
-
-app.listen(port, () => console.log("server running on port" + port))
+app.listen(port, () => console.log("server running on port" + port));
