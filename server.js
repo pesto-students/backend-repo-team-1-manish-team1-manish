9846fcb4248fc0cf57b1dc698dd7d98434a75046
SaurabhK05 <saurabhku@maqsoftware.com>
2023-07-14 15:49:13 +0530

    deployment commit

--- server.js
+++ server.js
@@ -44,4 +44,5 @@ app.use(authMiddleware.verifyToken);
 app.use("/auth", userAuthRouter);
 const callbackURL = process.env.CALLBACK_URL;
 console.log(callbackURL);
+
 app.listen(port, () => console.log("server running on port" + port));
