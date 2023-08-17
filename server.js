bc73ed2917c1d6dda2867a55f928e20c7d4562b2
RyuzaKiRa <90443131+RyuzaKiRa@users.noreply.github.com>
2023-08-17 13:25:05 +0530

    Update server.js

--- server.js
+++ server.js
@@ -11,9 +11,12 @@ const cors = require("cors");
 require("dotenv").config();
 const port = process.env.PORT || 5000;
 const app = express();
-app.use("*",
+app.use(
     cors({
-        origin: true
+        origin: 
+            process.env.NODE_ENV === "dev"
+                ? process.env.DEV_CORS_URL
+                : process.env.PROD_CORS_URL,
         credentials: true,
         optionSuccessStatus: 200,
         methods: "GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS",
