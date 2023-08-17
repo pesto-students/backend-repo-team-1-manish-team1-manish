f542b8fe56cca81923e511428a50d6f801860942
RyuzaKiRa <90443131+RyuzaKiRa@users.noreply.github.com>
2023-08-17 13:53:12 +0530

    Update OAuthRoute.js

--- Routes/OAuthRoute.js
+++ Routes/OAuthRoute.js
@@ -270,14 +270,13 @@ router.get("/logout", (req, res) => {
     //     sameSite: "none",
     //     overwrite: true,
     // });
-    res.cookie("jwtoken", '', {
+    return res.status(200).cookie("jwtoken", '', {
         expires: new Date(Date.now() - 2589200000000),
         httpOnly: true,
         secure: true,
         sameSite: 'none'
     });
     // res.send("<script>window.close()</script>");
-    return res.sendStatus(200);
 });
 
 module.exports = router;
